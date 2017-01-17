a40a3bafd432ff00a04ef5dc0d8308f561ca93c3
Nadav Blum <nadav@nite.org.il>
2017-01-17 14:16:00 +0200

    Add anchor element on error partial. Jump to error anchor when present. Add Hebrew translations.

--- app/assets/javascripts/rapidfire.js
+++ app/assets/javascripts/rapidfire.js
@@ -2,9 +2,21 @@
 //= require jquery_ujs
 //
 $(function() {
+ set_top_margin_on_questions_with_introduction();
+ jump_to_error_anchor();
+});
+
+function set_top_margin_on_questions_with_introduction() {
   $(".question:not(.kernel)").has(".introduction").each(function(){
      var q = $(this);
      var current_margin_top = parseInt(q.css("margin-top"));
      q.css("margin-top", (current_margin_top + 30) + 'px' );
   })
-});
+}
+
+
+function jump_to_error_anchor() {
+  if($("a[name='input_error']")[0]) {
+     location.hash = "#input_error"; 
+  }
+}
